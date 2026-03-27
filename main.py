c3e1db80b775ba107d0e6fcea0a4ad02ea3810a0
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2026-03-26 21:54:13 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from assalariado import Assalariado
+from horista import Horista
+
+trabalhadorUm = Assalariado(400, 10)
+trabalhadorDois = Horista(20, 50)
+
+trabalhadorUm.pagamentosAssalariados()
+trabalhadorDois.pagamentosHoristas()
+
